0ac564db0fe03396ad6f88cf8f7f83f8cc746fb3
unknown <anna.lalazarian@gmail.com>
2015-04-02 13:35:22 +0400

    changes

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 var express = require ("express");
-var config = require ("./config/conf.js");
-var bodyParser  = require('body-parser');
+var app = express();
+
+var bodyParser = require('body-parser');
+var config     = require ("./config/conf.js");
+
+/* load routers */ 
 var router1 = require("./routers/index");
 var router2 = require("./routers/register");
-var app=express();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 app.use("/public", express.static('./public'));
 
 
-app.use('/',router1);
-app.use('/registration',router2)
+app.use('/', router1);
+app.use('/registration', router2);
 
 app.listen(config.port);
 
--- config/conf.js
+++ config/conf.js
@@ -1,7 +1,7 @@
-var config={
-	port:3000,
-	mongo:"mongodb://localhost/user",
-	registration:"/registration",
+var config = {
+	port: 3000,
+	mongo: "mongodb://localhost/user",
+	registration: "/registration"
 };
 
-module.exports=config;
+module.exports = config;
